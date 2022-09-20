d10b6d9bd90a4119b09e33d499ec8c7e015ddee1
Oleksandr-Lytvyn <104520432+Oleksandr-Lytvyn@users.noreply.github.com>
2022-09-20 13:11:47 +0000

    Deploying to gh-pages from @ Oleksandr-Lytvyn/parcel-65@0461bdff6f1e7f9153777c697a75e28eed6ce249 🚀

---
+++ index.6fa2e857.js
@@ -0,0 +1,2 @@
+!function(){var o;o="yohoho!",console.log(o),function(o){console.log("tratata:".concat(o))}("uldumpt"),console.log("countries")}();
+//# sourceMappingURL=index.6fa2e857.js.map
---
+++ index.d6ff53dc.js
@@ -0,0 +1,2 @@
+var o;o="yohoho!",console.log(o),(o=>{console.log(`tratata:${o}`)})("uldumpt"),console.log("countries");
+//# sourceMappingURL=index.d6ff53dc.js.map
